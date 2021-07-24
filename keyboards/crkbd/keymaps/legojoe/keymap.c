aa29b8f501c0e1adf35c843739a2f584e7bffe81
Joe Chung <legojoe@gmail.com>
2021-07-24 15:54:56 +0100

    fix oled layer state

--- keyboards/crkbd/keymaps/legojoe/keymap.c
+++ keyboards/crkbd/keymaps/legojoe/keymap.c
@@ -104,10 +104,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 
-
-
-
-
 #ifdef OLED_DRIVER_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
   if (!is_keyboard_master()) {
@@ -116,27 +112,23 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
   return rotation;
 }
 
-#define L_BASE 0
-#define L_LOWER 2
-#define L_RAISE 4
-#define L_ADJUST 8
 
 void oled_render_layer_state(void) {
     oled_write_P(PSTR("Layer: "), false);
-    switch (layer_state) {
-        case L_BASE:
+    switch (get_highest_layer(layer_state)) {
+        case _QWERTY:
             oled_write_ln_P(PSTR("Default"), false);
             break;
-        case L_LOWER:
+        case _COLEMAK:
+            oled_write_ln_P(PSTR("Colemak"), false);
+            break;
+        case _LOWER:
             oled_write_ln_P(PSTR("Lower"), false);
             break;
-        case L_RAISE:
+        case _RAISE:
             oled_write_ln_P(PSTR("Raise"), false);
             break;
-        case L_ADJUST:
-        case L_ADJUST|L_LOWER:
-        case L_ADJUST|L_RAISE:
-        case L_ADJUST|L_LOWER|L_RAISE:
+        case _ADJUST:
             oled_write_ln_P(PSTR("Adjust"), false);
             break;
     }
