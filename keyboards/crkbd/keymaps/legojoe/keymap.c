123854d9e53ae8b902e3cd7a8b1a36789b0549d6
Joe Chung <legojoe@gmail.com>
2021-08-21 10:22:40 +0100

    tidy

--- keyboards/crkbd/keymaps/legojoe/keymap.c
+++ keyboards/crkbd/keymaps/legojoe/keymap.c
@@ -111,10 +111,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
   if (!is_keyboard_master()) {
     return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
-  } else {
-    return OLED_ROTATION_270;
   }
-  return rotation;
+  return OLED_ROTATION_270;
 }
 
 void oled_render_logo(void) {
