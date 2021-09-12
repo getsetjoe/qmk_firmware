a50cb305575dc97d20f07ea4fb4f8430b7b4425f
Joe Chung <legojoe@gmail.com>
2021-09-12 09:47:42 +0100

    add greater than

--- keyboards/crkbd/keymaps/legojoe/keymap.c
+++ keyboards/crkbd/keymaps/legojoe/keymap.c
@@ -33,6 +33,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MTCHBRK C(S(UK_BSLS))
 #define UNDO    C(KC_Z)
 #define REDO    C(KC_Y)
+#define GRT_THN S(KC_DOT)
 // Windows shortcuts
 #define VDESK_L G(C(KC_LEFT))
 #define VDESK_R G(C(KC_RIGHT))
@@ -77,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, UK_CIRC, UK_PIPE, KC_AMPR,  KC_EQL, KC_UNDS,                      KC_MINS, KC_LCBR, KC_RCBR, MOVE_DN, KC_COLN,  KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______,    REDO, UK_TILD, UK_HASH, KC_ASTR, UK_BSLS,                      KC_SLSH, KC_LBRC, KC_RBRC, _______, KC_QUES, _______,
+      _______,    REDO, UK_TILD, UK_HASH, KC_ASTR, UK_BSLS,                      KC_SLSH, KC_LBRC, KC_RBRC, GRT_THN, KC_QUES, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______,   MO(4), _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
