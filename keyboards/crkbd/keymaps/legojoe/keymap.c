2da7124d5a8a4a8b4c79fa9414dadca13a6f8935
Joe Chung <legojoe@gmail.com>
2021-11-10 09:53:04 +0000

    layer taps

--- keyboards/crkbd/keymaps/legojoe/keymap.c
+++ keyboards/crkbd/keymaps/legojoe/keymap.c
@@ -36,8 +36,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Windows shortcuts
 #define VDESK_L G(C(KC_LEFT))
 #define VDESK_R G(C(KC_RIGHT))
-// Layer-tap
-#define SPC_NAV LT(2, KC_SPC)
+// Layer-taps
+#define EQL_LOW LT(2, KC_EQL)
+#define TAB_RAI LT(3, KC_TAB)
 // Right-hand home row mods - lower layer
 #define P4_ALT LALT_T(KC_P4)
 #define P5_SFT RSFT_T(KC_P5)
@@ -72,11 +73,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC, KC_PGUP, KC_HOME,   KC_UP,  KC_END,    SAVE,                      KC_PLUS,   KC_P7,   KC_P8,   KC_P9, KC_PERC, _______,
+       KC_ESC, KC_PGUP, KC_HOME,   KC_UP,  KC_END, DELLINE,                      KC_PLUS,   KC_P7,   KC_P8,   KC_P9, KC_PERC, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, KC_PGDN, KC_LEFT, KC_DOWN,KC_RIGHT,  CTRL_D,                      KC_MINS,  P4_ALT,  P5_SFT,  P6_CTL, KC_COLN,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______,    UNDO,     CUT,    COPY,   PASTE, DELLINE,                        KC_P0,   KC_P1,   KC_P2,   KC_P3,  KC_DOT, _______,
+      _______,    UNDO,     CUT,    COPY,   PASTE,    SAVE,                        KC_P0,   KC_P1,   KC_P2,   KC_P3,  KC_DOT, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______,   MO(4), _______
                                       //`--------------------------'  `--------------------------'
@@ -114,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LALT,   MO(2),  KC_SPC,    SPC_NAV,   MO(3), KC_LGUI
+                                          KC_LALT, EQL_LOW,  KC_SPC,     KC_SPC, TAB_RAI, KC_LGUI
                                       //`--------------------------'  `--------------------------'
   )
 };
